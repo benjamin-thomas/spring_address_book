699e751364d69257d774830ff08d496f13911973
Benjamin <benjamin.guy.thomas@gmail.com>
2020-05-21 14:42:15 +0200

    Pass view param

--- src/main/java/invalid/bt/spring_address_book/controllers/HomeController.java
+++ src/main/java/invalid/bt/spring_address_book/controllers/HomeController.java
@@ -1,13 +1,18 @@
 package invalid.bt.spring_address_book.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(
+            @RequestParam(defaultValue = "John Doe") String name,
+            Model model) {
+        model.addAttribute("name", name);
         return "index";
     }
 }
