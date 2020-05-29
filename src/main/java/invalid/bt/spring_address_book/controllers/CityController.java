9e2624bb723285e993104a10f70f4611dbc0b96f
Benjamin <benjamin.guy.thomas@gmail.com>
2020-05-29 08:11:01 +0200

    Freemarker form

--- src/main/java/invalid/bt/spring_address_book/controllers/CityController.java
+++ src/main/java/invalid/bt/spring_address_book/controllers/CityController.java
@@ -2,11 +2,13 @@ package invalid.bt.spring_address_book.controllers;
 
 import invalid.bt.spring_address_book.forms.CityForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 // Per: https://spring.io/guides/gs/validating-form-input/
 @Controller
@@ -27,6 +29,26 @@ public class CityController {
         if (bindingResult.hasErrors()) {
             System.out.println(">>> cityForm = " + cityForm + ", bindingResult = " + bindingResult);
             return "thymeleaf/cities/form";
+        }
+
+        // Save stuff here, I'll do that later
+
+        return "redirect:/success";
+    }
+
+    @GetMapping("/freemarker/city/new")
+    public String showForm2(CityForm cityForm, Model model) {
+        model.addAttribute("city", new CityForm());
+        return "freemarker/cities/form";
+    }
+
+
+    @PostMapping("/freemarker/city")
+    public String createCity(@Valid CityForm city, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            System.out.println(">>> city = " + city + ", bindingResult = " + bindingResult);
+            model.addAttribute("city", city);
+            return "freemarker/cities/form";
         }
 
         // Save stuff here, I'll do that later
--- src/main/java/invalid/bt/spring_address_book/controllers/HomeController.java
+++ src/main/java/invalid/bt/spring_address_book/controllers/HomeController.java
@@ -23,4 +23,5 @@ public class HomeController {
         model.addAttribute("name", name);
         return "freemarker/index";
     }
+
 }
